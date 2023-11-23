356f7cd3ded7502b4ffa1ffe5442b960ad29dfdb
codexyan <mdcodeid@gmail.com>
2023-11-23 12:55:14 +0700

    this is empty template for vue 3 + tailwindcss november 2023

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1 class="text-3xl">Vue 3 + Tailwindcss</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
