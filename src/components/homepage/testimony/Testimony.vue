ba4338d588b5ceeab5e262cff6bd7de1a8d4a154
codexyan <mdcodeid@gmail.com>
2023-12-06 15:41:41 +0700

    update testimony mid

--- src/components/homepage/testimony/Testimony.vue
+++ src/components/homepage/testimony/Testimony.vue
@@ -97,7 +97,7 @@ const review = ref({
                                 class="rounded-full" />
                             <div>
                                 <h1 class="text-base text-white">{{ main.name }}</h1>
-                                <p class="italic font-thin text- text-slate-100">
+                                <p class="text-xs italic font-thin text-slate-100">
                                     {{ main.role }}
                                 </p>
                             </div>
