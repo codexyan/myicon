585e7db25cac3b2cd9ddd5efa08bf582e1be69a1
codexyan <mdcodeid@gmail.com>
2023-12-08 05:34:23 +0700

    update wedding view

--- src/components/weddingpage/hero/Hero.vue
+++ src/components/weddingpage/hero/Hero.vue
@@ -1,24 +1,21 @@
 <template>
     <div id="hero-banner">
-        <section class="bg-white h-[300px] md:h-[400px]">
-            <div class="grid items-center h-full max-w-screen-lg px-4 mx-auto lg:gap-12 lg:py-16 lg:grid-cols-12">
-                <div class="hidden rounded-sm lg:mt-0 lg:col-span-5 lg:flex ">
+        <section class="bg-white sm:h-[400px]">
+            <div class="grid items-center h-full max-w-screen-lg gap-8 px-4 mx-auto lg:gap-12 lg:py-16 lg:grid-cols-12">
+                <div class="rounded-sm lg:mt-0 lg:col-span-5">
                     <img src="../../../assets/img/banner/banner.png" alt="mockup" />
                 </div>
                 <div class="h-full mr-auto place-self-center lg:col-span-7">
                     <p class="text-amber-600">Deliver Your Timeless Moments</p>
                     <h1
-                        class="max-w-2xl mb-4 text-xl font-extrabold leading-none tracking-tight text-slate-800 md:text-5xl xl:text-3xl">
+                        class="max-w-2xl mb-4 text-xl font-extrabold leading-none tracking-tight text-slate-700 md:text-4xl">
                         Jasa Dokumentasi Pernikahan
                     </h1>
-                    <p class="max-w-2xl mb-6 font-light text-slate-600 lg:mb-8 md:text-base">
-                        Layanan jasa foto wedding Jogja, hadir sebagai sebagai spesialis
-                        dibidang fotografer pernikahan yang telah melayani sejak 2013 .
-                        Sebagai fotografer pernikahan, kami juga melayani jasa photography
-                        wedding sampai Internasional.
+                    <p class="max-w-2xl mb-6 text-sm font-light leading-6 text-slate-500 lg:mb-8 md:text-base">
+                        Layanan Dokuemntasi Pernikahan mulai dari Photoshoot, Cinmatic Videos dan juga menyediakan kebutuhan seperti bucket, hantaran, MUA dll. Didukungan peralatan, model, dan tim yang profesional.
                     </p>
                     <a href="#"
-                        class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center border rounded-md text-amber-600 hover:bg-amber-800 focus:ring-4 focus:ring-primary-300 border-amber-600 hover:text-white">
+                        class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center transition duration-200 ease-in-out delay-100 border rounded-md text-amber-600 hover:bg-amber-800 focus:ring-4 focus:ring-primary-300 border-amber-600 hover:text-white">
                         Pesan Jadwal
                         <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
                             xmlns="http://www.w3.org/2000/svg">
--- src/views/WeddingView.vue
+++ src/views/WeddingView.vue
@@ -1,9 +1,11 @@
 <script setup>
-    import Hero from '@/components/weddingpage/hero/Hero.vue';
+import Hero from '@/components/weddingpage/hero/Hero.vue';
+import Breadcumb from '../components/weddingpage/breadcumb/Breadcumb.vue';
 </script>
 
 <template>
-    <main class="bg-amber-100/10">
+    <main class="bg-amber-100/10 md:w-[72rem] w-full mx-auto md:py-6">
+        <Breadcumb />
         <Hero />
     </main>
 </template>
